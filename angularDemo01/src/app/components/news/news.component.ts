e5a75b8243cb663281b34c17405fa4c231721c22
mortalTOdeity <woairuidu@gmail.com>
2020-03-26 17:10:39 +0800

    new

--- angularDemo01/src/app/components/news/news.component.ts
+++ angularDemo01/src/app/components/news/news.component.ts
@@ -8,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class NewsComponent implements OnInit {
 
   public title = '我是一个新闻组件'
+  // 省略public 默认为public
+  msg = '我是新闻组件的msg'
 
-  constructor() { }
+  public name: string = '张三'
+
+  // 绑定任何类型
+  public item: any = 123
+
+  public userInfo: object = {
+    username: 'lizi',
+    age: 30
+  }
+
+  public message: any
+
+  constructor() {
+    this.message = 'the message'
+    console.log(this.message);
+    this.message = '我是改变后的message的值'
+  }
 
   ngOnInit(): void {
   }
