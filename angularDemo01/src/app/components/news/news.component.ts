ec9cfeb5c2337ba36c6bab33b46dae517cf13317
mortalTOdeity <woairuidu@gmail.com>
2020-03-26 16:07:22 +0800

    add comonents

---
+++ angularDemo01/src/app/components/news/news.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class NewsComponent implements OnInit {
+
+  public title = '我是一个新闻组件'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
