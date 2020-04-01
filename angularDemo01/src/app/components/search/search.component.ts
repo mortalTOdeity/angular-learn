478af96984187bb19fb5f035ce4ac69409b06d31
mortalTOdeity <woairuidu@gmail.com>
2020-04-01 19:16:10 +0800

    make

--- angularDemo01/src/app/components/search/search.component.ts
+++ angularDemo01/src/app/components/search/search.component.ts
@@ -6,14 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  public name: string = 'xiao'
+  public keywords: string
+  public historyList: Array<any> = []
 
-  public keywords: string[]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  doSearch() {
+  doAddBtn() {
+    if (this.historyList.indexOf(this.keywords) === -1) {
+      this.historyList.push({
+        title: this.keywords,
+        status: 0
+      })
+      this.keywords = ''
+    }
+  }
+  deleteHistory(key) {
+    this.historyList.splice(key, 1)
+  }
+  doAdd(code) {
+    if (code === 13) {
+      this.doAddBtn()
+    }
+  }
+  Printer() {
+    console.log(this.historyList);
 
   }
 
