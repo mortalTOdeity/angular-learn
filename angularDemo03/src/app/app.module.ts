4054f5c62b9a2b387c9ee4f1d61c5c92f4f9499d
mortalTOdeity <woairuidu@gmail.com>
2020-04-06 17:24:02 +0800

    add components

--- angularDemo03/src/app/app.module.ts
+++ angularDemo03/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
