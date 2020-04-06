a698c52c4bbf626297f7bb11cafc9b4f7de97d59
mortalTOdeity <woairuidu@gmail.com>
2020-04-06 17:28:10 +0800

    add router

--- angularDemo03/src/app/app-routing.module.ts
+++ angularDemo03/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'news', component: NewsComponent
+  },
+  {
+    path: 'product', component: ProductComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
